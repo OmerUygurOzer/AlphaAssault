8037fb40277e4f9caca82ef3327ad38fb66442e2
OmerUygurOzer <omer.u.ozer@gmail.com>
2016-01-08 14:58:13 -0800

    01/7/2016-PM(screenshots added)

--- mapeditor/src/MapEditorLauncher.java
+++ mapeditor/src/MapEditorLauncher.java
@@ -1,5 +1,8 @@
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import core.MapPanel;
+
 
 import javax.swing.*;
 
@@ -7,9 +10,9 @@ import javax.swing.*;
 /**
  * Created by Omer on 12/28/2015.
  */
-public class MapEditorLauncher extends JFrame {
+public class MapEditorLauncher extends JFrame{
     private static final String TITLE = "EnJine2D Level Editor";
-    private static final Vector2 APP_SIZE = new Vector2(1600,800);
+    private static final Vector2 APP_SIZE = new Vector2(1024,768);
 
     private MapPanel mapPanel;
 
@@ -20,13 +23,13 @@ public class MapEditorLauncher extends JFrame {
                 new Runnable() {
                     public void run() {
                         app.setVisible(true);
+
                     }
                 }
         );
     }
     public MapEditorLauncher() {
         super(TITLE);
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         mapPanel = new MapPanel();
@@ -36,8 +39,33 @@ public class MapEditorLauncher extends JFrame {
 
         setBounds(0,0,(int)APP_SIZE.x,(int)APP_SIZE.y);
 
+        JMenuBar menuBar = new JMenuBar();
+        JMenu file = new JMenu("File");
+        JMenu edit = new JMenu("Edit");
 
+        JMenuItem loadFile = new JMenuItem("Load Level");
+        JMenuItem saveFile = new JMenuItem("Save Level");
+        JMenuItem exit     = new JMenuItem("Exit");
+
+        JMenuItem undo = new JMenuItem("Undo");
+        JMenuItem redo = new JMenuItem("Redo");
+
+        file.add(loadFile);
+        file.add(saveFile);
+        file.add(exit);
+
+        edit.add(undo);
+        edit.add(redo);
+
+
+
+        menuBar.add(file);
+        menuBar.add(edit);
+
+
+        setJMenuBar(menuBar);
     }
+
 
 }
 
--- mapeditor/src/core/MapPanel.java
+++ mapeditor/src/core/MapPanel.java
@@ -1,20 +1,22 @@
 package core;
 
+
 import com.jogamp.opengl.GLException;
 import com.jogamp.opengl.awt.GLJPanel;
 
 /**
  * Created by Omer on 1/7/2016.
  */
-public class MapPanel extends GLJPanel {
+public class MapPanel extends GLJPanel{
 
 
     public MapPanel() throws GLException {
 
-
-    }
+      }
 
     public void draw(){
 
     }
+
+
 }
