05aa5318fd674f7d3bd1feae64469252d9a64c69
OmerUygurOzer <omer.u.ozer@gmail.com>
2016-01-19 01:14:24 -0800

    01/18/2016-PM(Worked on object editor. )

--- objectcreator/src/GUI/BaseEditor.java
+++ objectcreator/src/GUI/BaseEditor.java
@@ -1,15 +1,13 @@
 package GUI;
 
-import GUI.utils.ImageHolder;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Created by Omer on 1/17/2016.
@@ -20,7 +18,16 @@ public class BaseEditor extends JPanel implements ActionListener{
 
 
     private JButton addFrame = new JButton("Add Frame");
+    private JButton addCrossing = new JButton("Add");
+    private JButton removeCrossing = new JButton("Remove");
+    private List<String> crossingsAll = new ArrayList<String>();
+    private DefaultListModel<String> crossingListModel = new DefaultListModel<String>();
+    private JList<String> crossingsAllUI = new JList<String>(crossingListModel);
+
     private JComboBox<String> tileType = new JComboBox<String>();
+    private JComboBox<String> crossings = new JComboBox<String>();
+
+
 
     private int line = 0;
     private int lineSize = 20;
@@ -32,12 +39,39 @@ public class BaseEditor extends JPanel implements ActionListener{
 
 
         JLabel tileTypeLabel = new JLabel("Tile Type:");
-        tileTypeLabel.setBounds((WIDTH/2) - 60,lineSize*line,120,lineSize);line++;
+        tileTypeLabel.setBounds((WIDTH/2) - 60,lineSize*line,120,lineSize);
         add(tileTypeLabel);
-        tileType.setBounds((WIDTH/2) - 60,lineSize*line,120,lineSize);line++;
+        tileType.setBounds((WIDTH/2) + 60 ,lineSize*line,120,lineSize);line++;
         tileType.addItem("2D Tile");
         tileType.addItem("Isometric Tile");
         add(tileType);
+
+        JLabel crossingsLabel = new JLabel("Crossings:");
+        crossingsLabel.setBounds((WIDTH/2) - 60,lineSize*line,120,lineSize);
+        add(crossingsLabel);
+        crossings.setBounds((WIDTH/2) + 60 ,lineSize*line,120,lineSize);
+        crossings.addItem("N");
+        crossings.addItem("NE");
+        crossings.addItem("E");
+        crossings.addItem("SE");
+        crossings.addItem("S");
+        crossings.addItem("SW");
+        crossings.addItem("W");
+        crossings.addItem("NW");
+
+        addCrossing.setBounds((WIDTH/2) + 180 ,lineSize*line,120,lineSize); addCrossing.addActionListener(this);
+        removeCrossing.setBounds((WIDTH/2) + 300 ,lineSize*line,120,lineSize);removeCrossing.addActionListener(this);
+
+        JScrollPane listHolder = new JScrollPane();
+        listHolder.setBounds((WIDTH/2) + 420 ,lineSize*line,40,lineSize*crossings.getItemCount());
+        listHolder.setViewportView(crossingsAllUI);
+
+        add(crossings);
+        add(addCrossing);
+        add(removeCrossing);
+        add(listHolder);
+        line++;
+
         addFrame.setBounds((WIDTH/2) - 60,lineSize * line,120,lineSize); line++;
         addFrame.addActionListener(this);
         add(addFrame);
@@ -57,6 +91,28 @@ public class BaseEditor extends JPanel implements ActionListener{
             if(returnVal == JFileChooser.APPROVE_OPTION) {
               //DO STUFF
             }
+            return;
         }
+
+        if(e.getSource().equals(addCrossing)){
+            if(crossingsAll.contains((String)crossings.getSelectedItem())){
+                return;
+            }
+            crossingsAll.add((String)crossings.getSelectedItem());
+            crossingListModel.addElement((String)crossings.getSelectedItem());
+            System.out.println(crossingsAll);
+            return;
+        }
+
+        if(e.getSource().equals(removeCrossing)){
+            if(!crossingsAll.contains(crossings.getSelectedItem())){
+                return;
+            }
+            crossingsAll.remove(crossings.getSelectedItem());
+            crossingListModel.removeElement(crossings.getSelectedItem());
+            System.out.println(crossingsAll);
+            return;
+        }
+
     }
 }
